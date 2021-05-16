27f416c4d8f793e842653a37b444f373946ea483
kimtaemin <xoalstjtm@gmail.com>
2021-05-16 23:53:28 +0900

    1. 회원 등록 보완 2. 회원 조회 추가

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //스프링 컨테이너에서 스프링 빈이 관리 된다. ==> 스프링이 시작 될 때 Controller 어노테이션이 있는 아이들을 객체화 함.
 public class MemberController {
@@ -24,6 +27,8 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
+
+    //PostMapping은 data를 form에 넣어서 보냈을 때 해당 값들을 받기 위함.
     @PostMapping("members/new")
     public String create(MemberForm form) {
         Member member = new Member();
@@ -31,6 +36,16 @@ public class MemberController {
 
         memberService.join(member);
 
+        System.out.println("name:" + member.getName());
+
         return "redirect:/"; // 회원 등록 완료 => 홈 화면 전환
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", "members");
+        System.out.println(members);
+        return "members/memberList";
     }
 }
